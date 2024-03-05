6c620d4e156ce514a083761afcb8551f0eebb6a6
Shirakura-Kazuki <ok210192@oks.iput.ac.jp>
2024-03-05 23:18:36 +0900

    commitv3

---
+++ android/app/src/main/kotlin/com/example/gafa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gafa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(){    
+
+  const col = Column(
+    mainAxisSize: MainAxisSize.min,
+    // mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text('レモン'),
+    Text('リンゴ'),
+    Text('ブドウ'),
+  ],); 
+
+  const row = Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [col,col,col],
+  );
+
+  const a = MaterialApp(
+    home:Scaffold(
+      body:Center(
+        child: row,
+      ),
+    ),
+  );
+  runApp(a);
+}
